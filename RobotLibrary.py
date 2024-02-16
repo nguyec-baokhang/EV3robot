4836650886d8827035bd73aafec3ced2b21211b0
FuWag <116327453+FuWag@users.noreply.github.com>
2024-02-16 17:22:10 -0500

    change rotation stuff

--- RobotLibrary.py
+++ RobotLibrary.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor.lego import GyroSensor
 from ev3dev2.led import Leds
-
+import time
 #outputA is left motor
 #outputB is right motor
-
+gyro=""
+def Setup():
+    gyro=GyroSensor()
+    gyro.reset()
+    print("start calibrate")
+    gyro.calibrate()
+    print("done calibrate")
 #Move forwards a distance (cm)
 def Forward(distance):
-    vel=90.0/4   #inches per second
+    vel=45.0/2   #cm per second
     motors = MoveTank(OUTPUT_A,OUTPUT_B)
-    motors.on_for_rotations(SpeedPercent(-75), SpeedPercent(-75), distance/vel)
+    motors.on_for_rotations(SpeedPercent(-40), SpeedPercent(-40), distance/vel)
   
 #Move backwards a distance (cm)  
 def Reverse(distance):
-    vel=90.0/4   #inches per second
+    vel=45.0/2   #cm per second
     motors = MoveTank(OUTPUT_A,OUTPUT_B)
-    motors.on_for_rotations(SpeedPercent(75), SpeedPercent(75), distance/vel)
+    motors.on_for_rotations(SpeedPercent(40), SpeedPercent(40), distance/vel)
     
 #Rotate counter clock wise an angle (degree)
 def Rotate_CCW(angle):
-    ang_vel=309.0/2.0  #degs per second
-    motors = MoveTank(OUTPUT_A,OUTPUT_B)
-    motors.on_for_rotations(SpeedPercent(50), SpeedPercent(-50), angle/ang_vel)    
-    
+    #ang_vel=162.0  #degs per second
+
+    a=gyro.angle()
+    while ((gyro.angle()-a)<angle):
+        motors = MoveTank(OUTPUT_A,OUTPUT_B)
+        motors.on_for_rotations(SpeedPercent(50), SpeedPercent(-50), 0.1)#angle/ang_vel+0.3)    
+
+def get_Angle():
+    return gyro.angle
--- SubTask1B.py
+++ SubTask1B.py
@@ -3,9 +3,13 @@ import RobotLibrary as rl
 import time
 n=5
 distance=100  #cm
-rl.Setup
+rl.Setup()
 for i in range(0,n):
     rl.Forward(distance)
     time.sleep(0.5)
-    rl.Reverse(distance)
+    rl.Rotate_CCW(180)
+    time.sleep(0.5)
+    rl.Forward(distance)
+    time.sleep(0.5)
+    rl.Rotate_CCW(180)
     time.sleep(0.5)
