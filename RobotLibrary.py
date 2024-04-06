8dc7eff53d7f0a9d87e835f8d3d520444267336a
unknown <nguyecb@mail.uc.edu>
2024-04-06 18:03:32 -0400

    All subtask codes

--- RobotLibrary.py
+++ RobotLibrary.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C ,OUTPUT_D,SpeedPercent, MoveTank, MoveDifferential, SpeedRPM
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
-from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
+from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor,GyroSensor
 from ev3dev2.led import Leds
 from ev3dev2.wheel import EV3Tire,Wheel
+from ev3dev2.sound import Sound
 import time
 import os
-import numpy as np
+import math
 
 #outputA is left motor
 #outputB is right motor
@@ -24,27 +25,54 @@ mdiff = MoveDifferential(left_wheel, right_wheel, MyWheel, wheel_distance)
 
 isPicking=False
 
+gyro = GyroSensor()
+gyro.reset()
+gyro.calibrate()
+
+def inch_to_cm(inch):
+    return inch*2.54
+
+current_x=inch_to_cm(6)
+current_y=inch_to_cm(-6)
+current_angle=90
+
+def clamp_angle(angle_deg):
+    angle_deg = angle_deg + 360.0
+    mapped_angle = angle_deg % 360.0
+    return mapped_angle
+
+def say(sth):
+    spkr=Sound()
+    spkr.speak(sth)
+
+def updatePos(distance):
+    global current_x,current_y,current_angle
+    current_x+=distance*math.cos(math.radians(current_angle))
+    current_y+=distance*math.sin(math.radians(current_angle))
+
+def obstacle_detect():
+    us = UltrasonicSensor(INPUT_3)
+    distance = us.distance_centimeters
+    return distance
 #Move forwards a distance (cm)
 #Stop if there is an object 12.7 cm (5 inches) infront
-def Forward(distance):
-    #dist_moved=0.0
-    #step=0.5
-    #while dist_moved>=distance or (obstacle_detect()>12.7 and not isPicking):
-    #    mdiff.on_for_distance(SpeedRPM(-30),step*10)
-    #    dist_moved+=step
-    
-    mdiff.odometry_start(theta_degrees_start=90.0,x_pos_start=0.0,y_pos_start=0.0) 
-    mdiff.on_to_coordinates(SpeedRPM(-30), 0, distance*10, brake=True, block=False) #make sure block is False
+#Move forwards a distance (cm)
+#Stop if there is an object 12.7 cm (5 inches) infront
+def Forward(distance,speed=30,picking=False):
+    mdiff.odometry_start() 
+    mdiff.on_for_distance(SpeedRPM(-speed), distance*10, brake=True, block=False) #make sure block is False
     print("is Moving")
-    
-    while mdiff.y<distance*10:
+    say("bababooey")
+    while mdiff.is_running:
         print("distance in front: ",obstacle_detect())
-        if obstacle_detect()<=12.7:
-            mdiff.odometry_stop()
-            return
+        if not picking and obstacle_detect()<=22.7: #and not isPicking
+            print("stop")
+            break
         
-    mdiff.wait()
+    mdiff.stop()
     mdiff.odometry_stop()
+    updatePos(distance)
+    time.sleep(0.5)
     return
     
   
@@ -53,15 +81,29 @@ def Reverse(distance):
     mdiff.on_for_distance(SpeedRPM(30),distance*10)
 
 #Rotate counter clock wise an angle (degree)
-def Rotate_CCW(a):
-    mdiff.odometry_start(theta_degrees_start=0.0)
-    mdiff.turn_degrees(SpeedRPM(30),(a))
-    mdiff.odometry_stop()
-    angle+=a
-def obstacle_detect():
-    us = UltrasonicSensor()
-    distance = us.distance_centimeters
-    return distance
+def Rotate_CCW(angle):
+    global current_x,current_y,current_angle
+    
+    #mdiff.odometry_start(theta_degrees_start=0) 
+    #mdiff.turn_degrees(SpeedRPM(20),(angle),brake=True)
+    #mdiff.odometry_stop()
+    mult=0
+    if angle>0:
+        mult=1
+    else:
+        mult=-1
+    a=gyro.angle
+    motors = MoveTank(left_wheel,right_wheel)#angle/ang_vel+0.3) 
+    motors.on(SpeedRPM(10*mult), SpeedRPM(-10*mult))
+    while (abs(gyro.angle-a)<abs(angle)-10):
+        pass
+
+    motors.stop(brake=True)
+    current_angle+=angle
+    current_angle=clamp_angle(current_angle)  
+    
+    time.sleep(0.5)
+    print('rotated angle:'+str(angle))
 
 #1 is up
 #-1 is down  
@@ -119,49 +161,54 @@ cp = [6,-6]
 angle = 90
 
 print(shelf[1][1])
-shelf_choice = input("Input the shelf: ")
-box_choice = int(input("Input the box number: "))
+shelf_choice = "A1"
+box_choice = 3
 L_box = 36/6
+distance_x = 0
+distance_y = 0
 
-def moving_to_shelf_subtask1_(shelf_choice):
+def moving_to_shelf_subtask1_(shelf_choice,box_choice):
     for i in range (len(shelf)):
         if shelf_choice == shelf[i][1]: 
             if box_choice >= 1 and box_choice <7:
-                global distance_x
-                global distance_y
+                
                 distance_y = (shelf[i][0][1]-cp[1])*2.54
                 distance_x = (box_choice*L_box+3)*2.54
                 current_x = distance_x + cp[0]
                 current_y = distance_y + cp[1]
                 Forward(distance_y)
-                Rotate_CCW(90)
+                Rotate_CCW(-90)
                 Forward(distance_x)
                 time.sleep(5)
-                Forward(102*2.54-current_x)
-                Rotate_CCW(90)
-                Forward(distance_y*2.54)
+                Forward(102*2.54-current_x-10)
+                Rotate_CCW(-90)
+                Forward(distance_y)
             else:
-                global distance_x
-                global distance_y
+                
                 distance_y = (shelf[i][0][1]-cp[1]+12)*2.54
                 distance_x = (box_choice-7+3)*2.54
                 current_x = distance_x + cp[0]
                 current_y = distance_y + cp[1]
                 Forward(distance_y)
-                Rotate_CCW(90)
+                Rotate_CCW(-90)
                 Forward(distance_x)
                 time.sleep(5)
                 Forward(102*2.54-current_x)
-                Rotate_CCW(90)
+                Rotate_CCW(-90)
                 Forward(distance_y)
 
+    global current_distance_x
+    global current_distance_y
+    current_distance_x = distance_x
+    current_distance_y = distance_y
+
 def moving_back_subtask2():
-    Rotate_CCW(180)
-    Forward(distance_y)
-    Rotate_CCW(-90)
+    Rotate_CCW(-180)
+    Forward(current_distance_y)
+    Rotate_CCW(90)
     Forward((102-6)*2.54)
-    Rotate_CCW(-90)
-    Forward(distance_y)
+    Rotate_CCW(90)
+    Forward(current_distance_y)
 
 def subtask3_subtask4():
     global color_read
@@ -172,25 +219,27 @@ def subtask3_subtask4():
     else:
         distance_x = (box_choice-12)
         Forward(distance_x)
-
-    for i in range (2):
+    Forward(1.27)
+    for i in range (4):
         color = readBnW(input=ColorSensor)
         print("The color is: " + str(color))
-        Forward(0.5*2.54)
+        Forward(1.27)
         color_read.append(color)
 
-    Rotate_CCW(90)
-    Forward(12.8)
-    moveForklift(1)
     Rotate_CCW(-90)
+    moveForklift(1)
+    Rotate_CCW(180)
+    Forward(5)
+    moveForklift(-1)
+    moveForklift(1)
 
     new_distance_x = 36 - distance_x
-    Forward(new_distance_x)
+    Forward(new_distance_x*2.54)
     moveForklift(-1)
 
 
            
-
+moving_to_shelf_subtask1_(shelf_choice,box_choice)
 
 
 
